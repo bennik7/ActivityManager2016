8823c47eb8d00664b8c5b11e1366e29b52d5c1c7
bennik7 <benedikt.k2198@gmail.com>
2016-05-31 10:36:56 +0200

    Initial commit

---
+++ app/src/main/java/config/Config.java
@@ -0,0 +1,20 @@
+package config;
+
+public class Config {
+
+    final public static String CLUSTERURL = "http://ortc-developers.realtime.co/server/2.1/";
+    //final public static String CLUSTERURL = null;
+    final public static String URL = null;
+    final public static String METADATA = "androidApp";
+    final public static String TOKEN = "token";
+    final public static String APPKEY = "nlnKgd";
+    final public static String PROJECT_ID = "22546059885";
+
+
+    final public static int CHANNEL_NAME = 1;
+    final public static int CHANNEL_ADD = 2;
+    final public static int CHANNEL_DEL = 3;
+    final public static int MSG_SIZE = 260;
+
+    final public static String DATE_FORMAT = "yyyy-MM-dd HH:mm";
+}
